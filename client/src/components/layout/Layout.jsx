ec0419ccaea37a6c5dd00c08c893226e716d4dd5
Stephen Sayage <stephensayage@gmail.com>
2020-10-22 13:41:55 -0400

    burger ingredients added and css finished

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import classes from "./Layout.css";
 
 export default function Layout(props) {
   return (
     <Aux>
       <div>Toolbar, sidedrawer, backdrop</div>
-      <main>{props.children}</main>
+      <main className={classes.Container}>{props.children}</main>
     </Aux>
   );
 }
